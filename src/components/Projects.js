a6ccc4dc7846d74b12483fd8bcba1d5be26d2be4
Eliam Ramirez <santiago.rg@live.com>
2023-03-25 00:13:34 -0600

    Load More Button

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
+import {useState} from 'react'
 
 //ELLIE
 import Login from "../assets/img/Ellie-Login.jpg" ;
@@ -22,32 +23,46 @@ import Morse from "../assets/img/Morse.jpg";
 import CRUDjsp from "../assets/img/CRUDjsp.png";
 import Tickets from "../assets/img/BisonBillet.jpg";
 import Pharmacy from "../assets/img/pharmacy.jpg";
+import Anova from  "../assets/img/ANOVA.png";
 
 import GaussImg from "../assets/img/gauss-jordan.png";
 import FalsePositionImg from "../assets/img/reglaFalsa.png";
 import InterpolationImg from "../assets/img/InterpolacionUnico.png";
 
+import Tsp from "../assets/img/TSPCapture.jpg";
+
+
+
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
 import { useTranslation } from "react-i18next";
+
+
+
 export const Projects = () => {
   const [t,i18n]=useTranslation("global");
+
+   var projects1Shown=3;
+   var projects2Shown=3;
+   var projects3Shown=3;
+
   const SocialMediaProject = [
     {
       title: t("projects.Ellie-Login"),
       description: "Angular/Ionic, Firebase",
       imgUrl: Login,
     },
-    {
-      title: t("projects.Ellie-CreateAccount"),
-      description: "Angular/Ionic, Firebase",
-      imgUrl: CreateAccount,
-    },
+    
     {
       title: t("projects.Ellie-Home"),
       description: "Angular/Ionic, Firebase",
       imgUrl: Home,
+    },
+    {
+      title: t("projects.Ellie-Profile"),
+      description: "Angular/Ionic, Firebase",
+      imgUrl: Profile,
     },
     {
       title: t("projects.Ellie-Messages"),
@@ -59,11 +74,7 @@ export const Projects = () => {
       description: "Angular/Ionic, Firebase",
       imgUrl: Post,
     },
-    {
-      title: t("projects.Ellie-Profile"),
-      description: "Angular/Ionic, Firebase",
-      imgUrl: Profile,
-    },
+    
     {
       title: t("projects.Ellie-Notifications"),
       description: "Angular/Ionic, Firebase",
@@ -80,6 +91,11 @@ export const Projects = () => {
       imgUrl: License,
     },
 
+    {
+      title: t("projects.Ellie-CreateAccount"),
+      description: "Angular/Ionic, Firebase",
+      imgUrl: CreateAccount,
+    },
   ];
 
   const Mobileprojects = [
@@ -100,10 +116,28 @@ export const Projects = () => {
     }
   ];
   const Desktopprojects = [
+    
+    
     {
-      title: t("projects.Math1"),
+      title: t("projects.Desktop2"),
+      description: "Java Server Pages, MySQL",
+      imgUrl: CRUDjsp,
+    },
+    {
+      title: t("projects.Tsp"),
+      description: "Angular/Ionic",
+      imgUrl: Tsp,
+    },
+    {
+      title: "ANOVA",
       description: "WinForms C#",
-      imgUrl: GaussImg,
+      imgUrl: Anova,
+    },
+    
+    {
+      title: t("projects.Math3"),
+      description: "WinForms C#, AngouriMath,  WPF Math",
+      imgUrl: InterpolationImg,
     },
     {
       title: t("projects.Math2"),
@@ -111,36 +145,74 @@ export const Projects = () => {
       imgUrl: FalsePositionImg,
     },
     {
-      title: t("projects.Math3"),
-      description: "WinForms C#, AngouriMath,  WPF Math",
-      imgUrl: InterpolationImg,
-    },
-    {
       title: t("projects.Desktop1"),
       description: "WinForms C#, Bunifu, Microsoft SQL Server",
       imgUrl: Tickets,
     },
-    {
-      title: t("projects.Desktop2"),
-      description: "Java Server Pages, MySQL",
-      imgUrl: CRUDjsp,
-    },
+    
     {
       title: t("projects.Desktop3"),
       description: "Java, Swing, MySQL",
       imgUrl: Pharmacy,
-    }
+    },
+    {
+      title: t("projects.Math1"),
+      description: "WinForms C#",
+      imgUrl: GaussImg,
+    },
+    
+    
   ];
+
+
+  const imagePerRow = 3;
+
+  const [next1, setNext1] = useState(imagePerRow);
+  const handleMoreImage1 = () => {
+      setNext1(next1 + imagePerRow);
+    };
+
+    const [next2, setNext2] = useState(imagePerRow);
+    const handleMoreImage2 = () => {
+        setNext2(next2 + imagePerRow);
+      };
+
+    const [next3, setNext3] = useState(imagePerRow);
+    const handleMoreImage3 = () => {
+        setNext3(next3 + imagePerRow);
+      };
+
+  function LoadMore(i) {
+    switch (i) {
+      case 1:
+        
+        projects1Shown+=3;
+        alert(projects1Shown);
+        break;
+      case 2:
+        projects2Shown+=3;
+        alert(projects2Shown);
+        break;
+      case 3:
+        projects3Shown+=3;
+        alert(projects3Shown);
+      break;
+      default:
+        alert("ERROR");
+    }
+  }
 
   return (
     <section className="project" id="project">
       <Container>
         <Row>
+          
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>{t("navbar.projects")}</h2>
+                
                 <p>{t("projects.intro")}</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
@@ -159,21 +231,27 @@ export const Projects = () => {
                     
                     <Row>
                         {
-                          SocialMediaProject.map((project, index) => {
+                          SocialMediaProject.slice(0, next1)?.map((project, index) => {
                             return (
                               <ProjectCard
                                 key={index}
                                 {...project}
+                                
                                 />
+                                
                             )
                           })
                         }
+                        <button onClick={handleMoreImage1}>
+                          <span>{t("projects.LoadMore")}</span>
+                        </button> 
                       </Row>
+                      
                     </Tab.Pane>
                     <Tab.Pane eventKey="second">
                     <Row>
                         {
-                          Mobileprojects.map((project, index) => {
+                          Mobileprojects.slice(0, next2)?.map((project, index) => {
                             return (
                               <ProjectCard
                                 key={index}
@@ -182,12 +260,16 @@ export const Projects = () => {
                             )
                           })
                         }
+                        <button onClick={handleMoreImage2}>
+                          <span>{t("projects.LoadMore")}</span>
+                        </button> 
                       </Row>
+                      
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
                     <Row>
                         {
-                          Desktopprojects.map((project, index) => {
+                          Desktopprojects.slice(0, next3)?.map((project, index) => {
                             return (
                               <ProjectCard
                                 key={index}
@@ -196,11 +278,15 @@ export const Projects = () => {
                             )
                           })
                         }
+                        <button onClick={handleMoreImage3}>
+                          <span>{t("projects.LoadMore")}</span>
+                        </button> 
                       </Row>
                       
                     </Tab.Pane>
                   </Tab.Content>
                 </Tab.Container>
+                
               </div>}
             </TrackVisibility>
           </Col>
